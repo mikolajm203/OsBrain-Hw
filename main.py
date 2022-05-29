29be47e7bb0f129a644f99377157bff9b9730274
Mikołaj Stańczyk <62181873+mikolajm203@users.noreply.github.com>
2022-05-29 21:40:53 +0200

    The solution

--- main.py
+++ main.py
@@ -1,16 +1,101 @@
-# This is a sample Python script.
+import time
+import random
+from osbrain import Agent, run_nameserver, run_agent
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+TOPICS = ['Food', 'Movies', 'Sport', 'Traveling', 'Music', 'Games', 'Books']
+NAMES = ['Marie', 'John', 'Nicolas', 'Paul', 'Martha', 'Natalie', 'Matt']
 
 
-# Press the green button in the gutter to run the script.
+class SpeedDateOrganizer(Agent):
+    pairs = []
+
+    def on_init(self):
+        print("The Speeddating event has begun!")
+        self.hasEnded = False
+
+    def shuffle_pairs(self):
+        print('Changing the pairs...')
+        random.shuffle(pairs)
+        self.send('OrganizerChannel', self.pairs)
+
+
+class DateResponder(Agent):
+    def on_init(self):
+        self.Name = random.choice(NAMES)
+        self.topics = random.sample(TOPICS, 2)
+        print("DateResponder " + self.Name + " initiated with topics: " + str(self.topics))
+
+    def set_address(self, id):
+        self.set_attr(address=self.bind('REP', str(id), handler=self.answer_question))
+        return self.address
+
+    def answer_question(self, msg):
+        if msg in self.topics:
+            return 'yes'
+        return 'no'
+
+
+class DateInitiator(Agent):
+
+    def on_init(self):
+        self.Name = random.choice(NAMES)
+        self.topics = random.sample(TOPICS, 2)
+        print("DateInitiator " + self.Name + " initiated with topics: " + str(self.topics))
+
+def start_date(agent, pairs):
+    table = pairs[agent.id]
+    index = random.randint(0, 1)
+    agent.send(str(table), agent.topics[index])
+    response = agent.recv(str(table))
+    if response == 'yes':
+        agent.send('report', agent.Name + " Found love of his/her life because of interest: " + agent.topics[index])
+
+def end_event(agent, message):
+    print(message)
+    agent.hasEnded = True
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Init variables
+    nameServer = run_nameserver()
+    responders = []
+    initiators = []
+    addresses = []
+    noOfPairs = 5
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # shuffle initial pairs
+    pairs = []
+    for i in range(noOfPairs):
+        pairs.append(i)
+    random.shuffle(pairs)
+
+    # init organizer
+    organizer = run_agent('Organizer', base=SpeedDateOrganizer)
+    organizer.set_attr(pairs=pairs)
+    addressToSubscribe = organizer.bind('PUB', alias='OrganizerChannel')
+
+    # init participants
+    for i in range(noOfPairs):
+        # Responders
+        responder = run_agent('Responder' + str(i), base=DateResponder)
+        responder.set_attr(id=i)
+        responders.append(responder)
+        responder.set_address(i)
+        addresses.append(responder.get_attr('address'))
+
+        # Initiators
+        initiator = run_agent('Initiator' + str(i), base=DateInitiator)
+        initiator.set_attr(id=i)
+        initiators.append(initiator)
+        address = initiator.bind('PUSH', alias='report')
+        organizer.connect(address, handler=end_event)
+        initiator.connect(addressToSubscribe, handler=start_date)
+    # connect each initiator to each responder address
+    for agent in initiators:
+        for i in range(len(addresses)):
+            agent.connect(addresses[i], alias=str(i))
+
+    # main loop
+    while not organizer.get_attr('hasEnded'):
+        organizer.shuffle_pairs()
+        time.sleep(3)
+    nameServer.shutdown()
